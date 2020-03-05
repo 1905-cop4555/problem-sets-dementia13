302bbe1096f239e9249c51d1220cae38478e3330
Sean Quinn <s_quinn@bellsouth.net>
2020-03-05 12:56:22 -0500

    Slight progress on problem 2 unions

--- Coordinates.fsx
+++ Coordinates.fsx
@@ -2,3 +2,7 @@ type 'a Coordinates =
     | Tuple of val1: 'a * val2: 'a
     | Threeple of val1: 'a * val2: 'a * val3: 'a
     | Fourple of val1: 'a * val2: 'a * val3: 'a * val4: 'a
+
+let Tuple = (4 , 20) // :)
+let Threeple = (3.2, 151.0, 97.8)
+let Fourple = ("Farcical aquatic ceremony", "Judean People's Front", "Scott of the Antarctic", "Grale frit")
