495e9478f1cebebb0fcdfaa44dc7521b224c626e
Sean Quinn <s_quinn@bellsouth.net>
2020-03-05 12:02:42 -0500

    Finally found the trick to specify pattern-matched union types

--- Coordinates.fsx
+++ Coordinates.fsx
@@ -1,4 +1,4 @@
-type Coordinates =
-    | Tuple of (Tup1,  Tup2)
-    | Threeple
-    | Fourple 
+type 'a Coordinates =
+    | Tuple of val1: 'a * val2: 'a
+    | Threeple of val1: 'a * val2: 'a * val3: 'a
+    | Fourple of val1: 'a * val2: 'a * val3: 'a * val4: 'a
