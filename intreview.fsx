7d4d980aebbf108be032c3d38f4239ab2065165b
Sean Quinn <s_quinn@bellsouth.net>
2020-03-14 12:58:15 -0400

    reviewing more early stuff

--- intreview.fsx
+++ intreview.fsx
@@ -1,2 +1,8 @@
 printfn "%s" "Hello world!"
 
+let prompt = "Hey, kittycat. What is your name?"
+let retort = "Kittycat, your name is "
+printfn "%s" prompt
+let name = System.Console.ReadLine()
+printfn "%s %s" retort name
+
