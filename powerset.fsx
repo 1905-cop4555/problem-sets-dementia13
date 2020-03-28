21633f23c24fe08bb3d5dcb0d4ec17c4eb18cac4
Sean Quinn <s_quinn@bellsouth.net>
2020-03-28 18:17:36 -0400

    finally got the powerset function to work

--- powerset.fsx
+++ powerset.fsx
@@ -1,5 +1,13 @@
 let lx = [1; 2; 3]
+
+
+let rec current = function
+    | [] -> [[]]
+    | x::xs -> [x]
+
 let rec powerset = function
-    | [] -> []
-    | x::xs -> let partial = powerset(xs)
-                let current = List.map (fun  -> x::xs) partial
+    | [] -> [[]]
+    | x::xs -> let partial = powerset xs
+               List.map (fun xs -> x::xs) partial @ partial
+
+powerset lx
